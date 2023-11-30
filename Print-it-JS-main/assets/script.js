f835f10a216130b3cd8a5c6a6a6a32a7dde601d5
Zinedine Chami <zinedine.chami@gmail.com>
2023-11-30 01:08:54 +0100

    added banner img function for first slideshow

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -19,8 +19,25 @@ const slides = [
 ]
 
 // etape 2 ajoutez des event listeners au fleches
-let fleche_gauche = document.querySelector("#banner .arrow_left").addEventListener("click")
-console.log (fleche_gauche)
+let fleche_droite = document.querySelector("#banner .arrow_right") ;
 
-let fleche_droite = document.querySelector("#banner .arrow_right").addEventListener("click")
-console.log (fleche_droite)
+fleche_droite.addEventListener("click", changeImage );
+
+// fleche gauche
+
+let fleche_gauche = document.querySelector("#banner .arrow_right") ;
+
+fleche_gauche.addEventListener("click", () => {  
+	
+console.log ("fleche_gauche") 
+});
+
+
+// etape 4
+
+let banner_img = document.querySelector ("#banner .banner-img")
+
+
+function changeImage () { 
+	banner_img.setAttribute("src", "./assets/images/slideshow/slide2.jpg");
+}
