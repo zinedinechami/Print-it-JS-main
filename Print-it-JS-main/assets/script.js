0c0a9f078dc42b2ea310511f01bfd1cd11323dc2
Zinedine Chami <zinedine.chami@gmail.com>
2023-12-01 22:14:19 +0100

    change function names, made next and prev functions able to navigate slides array index

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,3 +1,6 @@
+let banner_img = document.querySelector ("#banner .banner-img")
+
+let banner_tagLine = document.querySelector ("#banner .tagLine")
 
 const slides = [
 	{
@@ -16,32 +19,37 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+] ;
 
-// etape 4
-let banner_img = document.querySelector ("#banner .banner-img")
+var i = 0;
 
-let banner_tagLine = document.querySelector ("#banner .tagLine")
-
-let bullet_point = document.querySelector (".dot-1")
-
-function changeImage () { 
-	banner_img.setAttribute("src", "./assets/images/slideshow/slide2.jpg");
-	document.querySelector ("#banner .tagLine").innerHTML = "Tirages haute définition grand format <span>pour vos bureaux et events</span>" ; 
+function prev () {
+	if (i <= 0 ) i = slides.length;
+	i--; 
+	return setImg();
 }
 
+function next () { 
+	if ( i >= slides.length-1) i = -1;
+	i++;
+	return setImg();
+} ;
+
+function setImg () {
+	return banner_img.setAttribute('src', './assets/images/slideshow/' + slides.image[i]);
+}
 
 
 // etape 2 ajoutez des event listeners au fleches
 let fleche_droite = document.querySelector("#banner .arrow_right") ;
 
-fleche_droite.addEventListener("click", changeImage );
+fleche_droite.addEventListener("click", next );
 
 // fleche gauche
 
-let fleche_gauche = document.querySelector("#banner .arrow_right") ;
+let fleche_gauche = document.querySelector("#banner .arrow_left") ;
 
-fleche_gauche.addEventListener("click", changeImage );
+fleche_gauche.addEventListener("click", prev );
 
 
 
