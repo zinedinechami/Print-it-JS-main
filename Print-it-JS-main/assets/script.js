5267de8068d1fb9e9958da3855b13a49a7ba2598
Zinedine Chami <zinedine.chami@gmail.com>
2023-12-03 00:36:14 +0100

    seperated array into 2, cleaned script.js up, modified setImg function to allow the modification of the Tagline

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,53 +1,49 @@
-let banner_img = document.querySelector ("#banner .banner-img")
+// variables
+let banner_img = document.querySelector ("#banner .banner-img") ;
 
-let banner_tagLine = document.querySelector ("#banner .tagLine")
+let banner_tagLine = document.querySelector ("#banner .tagLine") ;
 
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-] ;
+let fleche_droite = document.querySelector("#banner .arrow_right") ;
 
-var i = 0;
+let fleche_gauche = document.querySelector("#banner .arrow_left") ;
+
+// data for the carrousel
+
+const images = [
+	"slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.png",
+]
+
+const tagLine = [
+	"Impressions tous formats <span>en boutique et en ligne</span>", 
+	"Tirages haute définition grand format <span>pour vos bureaux et events</span>", 
+	"Grand choix de couleurs <span>de CMJN aux pantones</span>",
+	"Autocollants <span>avec découpe laser sur mesure</span>",
+]
+
+// slider id
+let i = 0;
 
 function prev () {
-	if (i <= 0 ) i = slides.length;
+	if (i <= 0 ) i = images.length;
 	i--; 
 	return setImg();
 }
 
 function next () { 
-	if ( i >= slides.length-1) i = -1;
+	if ( i >= images.length-1) i = -1;
 	i++;
 	return setImg();
 } ;
 
 function setImg () {
-	return banner_img.setAttribute('src', './assets/images/slideshow/' + slides.image[i]);
+	banner_img.setAttribute('src', './assets/images/slideshow/' + images[i]);
+	banner_tagLine.innerHTML = tagLine[i];
 }
 
 
-// etape 2 ajoutez des event listeners au fleches
-let fleche_droite = document.querySelector("#banner .arrow_right") ;
+// event listerners
 
 fleche_droite.addEventListener("click", next );
-
-// fleche gauche
-
-let fleche_gauche = document.querySelector("#banner .arrow_left") ;
 
 fleche_gauche.addEventListener("click", prev );
 
