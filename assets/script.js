9c5ee69f8bdea56ce66d157b1c983122f8eda632
Zinedine Chami <zinedine.chami@gmail.com>
2023-12-06 21:48:28 +0100

    removed individual bullet points from html, they are now added per each slides array element

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
+
+
+// create bullet points
+
 // variables
+
 let banner_img = document.querySelector ("#banner .banner-img") ;
 
 let banner_tagLine = document.querySelector ("#banner .tagLine") ;
@@ -7,27 +12,49 @@ let fleche_droite = document.querySelector("#banner .arrow_right") ;
 
 let fleche_gauche = document.querySelector("#banner .arrow_left") ;
 
-let dots = document.getElementsByClassName("dot")
+let dots = document.getElementsByClassName("dot") ;
+
+
 
 // tableaux pour le carrousel
 
-const images = [
-	"slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.png",
-]
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+] ;
 
-const tagLine = [
-	"Impressions tous formats <span>en boutique et en ligne</span>", 
-	"Tirages haute définition grand format <span>pour vos bureaux et events</span>", 
-	"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-	"Autocollants <span>avec découpe laser sur mesure</span>",
-]
 
+
+slides.forEach(image => {
+	let individualDot = document.createElement('div');
+	individualDot.classList.add('dot');
+	let dotSection = document.querySelector(".dot-section")
+	dotSection.appendChild(individualDot);
+});
+
+dots[0].classList.add("dot_selected");
 
 
 // Event listerners pour les fleches
 fleche_droite.addEventListener("click", next );
 
 fleche_gauche.addEventListener("click", prev );
+
+
 
 
 
@@ -38,7 +65,7 @@ let i = 0;
 // Défilement infini grace au conditions
 function prev () {
 	// on declaire que si l'index des images est moins que 0
-	if (i <= 0 ) i = images.length; 
+	if (i <= 0 ) i = slides.length; 
 	// on le decrement de 1 pour revenir au dernier index du tableau
 	i--; 
 	// puis on retourne cet fonction pour mettre a jour l'image correspondant à l'index du array images fixée par la variable i
@@ -48,7 +75,7 @@ function prev () {
 
 function next () { 
 	// si l'index i est supérieur au dernier index du tableau, on fixe -1 pour revenir au premier index du tableau
-	if ( i >= images.length-1) i = -1; 
+	if ( i >= slides.length-1) i = -1; 
 	// on l'incremente de 1 pour aller se déplacer au prochain index du tableau à partir du premier index
 	i++;
 	// puis on retourne cet fonction pour mettre a jour l'image correspondant à l'index du array images fixée par la variable i
@@ -59,9 +86,9 @@ function next () {
 
 // Modifiez le slide au clic du bouton
 function setImg () {
-	banner_img.setAttribute('src', './assets/images/slideshow/' + images[i]);
+	banner_img.setAttribute('src', './assets/images/slideshow/' + slides[i].image);
 	// tagLine utlise le meme index que le tableau image pour se referer au bon texte par rapport a l'image
-	banner_tagLine.innerHTML = tagLine[i];
+	banner_tagLine.innerHTML = slides[i].tagLine;
 	// for loop
 	// d'abord on initialise une index pour les dots
 	// puis on initialise une condition, si d est inferieur a longeur des dots
@@ -71,4 +98,5 @@ function setImg () {
     }
 	// met a jour le bullet point part rapport a l'index i correspondant a l'image
 	dots[i].classList.add("dot_selected");
-}
+} ;
+
